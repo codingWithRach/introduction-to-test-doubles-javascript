c905649373d30f5ade749acd94951ed54f544cc5
Rachel Miles <rachmiles@gmail.com>
2022-03-10 10:52:45 +0000

    Change bubble_tea_order_service.test.js to use test parameterisation

--- src/bubble_tea_order_service.test.js
+++ src/bubble_tea_order_service.test.js
@@ -1,17 +1,17 @@
-const {createOrderRequest} = require('./bubble_tea_order_service');
-const bubbleTeaType = require('./bubble_tea_type');
-const bubbleTeaMessenger = require('./bubble_tea_messenger');
-jest.mock('./bubble_tea_messenger');
-jest.mock('./simple_logger');
+const { createOrderRequest } = require("./bubble_tea_order_service");
+const bubbleTeaType = require("./bubble_tea_type");
+const bubbleTeaMessenger = require("./bubble_tea_messenger");
+jest.mock("./bubble_tea_messenger");
+jest.mock("./simple_logger");
 
 let dummyPaymentDetails;
 
 beforeEach(() => {
   dummyPaymentDetails = {
-    name: 'Some person',
-    address: '123 Some Street',
+    name: "Some person",
+    address: "123 Some Street",
     debitCard: {
-      digits: '123456',
+      digits: "123456",
     },
   };
 });
@@ -20,23 +20,28 @@ afterEach(() => {
   jest.clearAllMocks();
 });
 
-test('test successful bubble tea order request', () => {
-  // Arrange
-  const bubbleTeaRequest = {
-    paymentDetails: dummyPaymentDetails,
-    bubbleTea: {
-      type: bubbleTeaType.MATCHAMILKTEA,
-    },
-  };
+test.each([[bubbleTeaType.MATCHAMILKTEA], [bubbleTeaType.LYCHEEICETEA]])(
+  "test successful order request for bubble %p",
+  (teaType) => {
+    // Arrange
+    const bubbleTeaRequest = {
+      paymentDetails: dummyPaymentDetails,
+      bubbleTea: {
+        type: teaType,
+      },
+    };
 
-  // Act
-  const orderRequest = createOrderRequest(bubbleTeaRequest);
+    // Act
+    const orderRequest = createOrderRequest(bubbleTeaRequest);
 
-  // Assert
-  expect(orderRequest.name).toBe(dummyPaymentDetails.name);
-  expect(orderRequest.digits).toBe(dummyPaymentDetails.debitCard.digits);
-  expect(bubbleTeaMessenger.sendBubbleTeaOrderRequestEmail)
-      .toHaveBeenCalledWith(orderRequest);
-  expect(bubbleTeaMessenger.sendBubbleTeaOrderRequestEmail)
-      .toHaveBeenCalledTimes(1);
-});
+    // Assert
+    expect(orderRequest.name).toBe(dummyPaymentDetails.name);
+    expect(orderRequest.digits).toBe(dummyPaymentDetails.debitCard.digits);
+    expect(
+      bubbleTeaMessenger.sendBubbleTeaOrderRequestEmail
+    ).toHaveBeenCalledWith(orderRequest);
+    expect(
+      bubbleTeaMessenger.sendBubbleTeaOrderRequestEmail
+    ).toHaveBeenCalledTimes(1);
+  }
+);
